5cf3540e8bdbade766f2b28dac56c13071356208
DemoXin <DemoXin@demoxin.com>
2014-07-24 08:43:32 -0400

    * Corrected version strings * Updated MCMOD.INFO

--- src/main/java/com/demoxin/minecraft/simpleenderthings/SimpleEnderThings.java
+++ src/main/java/com/demoxin/minecraft/simpleenderthings/SimpleEnderThings.java
@@ -18,7 +18,7 @@ public class SimpleEnderThings {
 	// Mod Info
 	public static final String MODID = "SimpleEnderThings";
 	public static final String NAME = "SimpleEnderThings";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.2.1";
 	// Mod Info End
 	
 	// Singleton
